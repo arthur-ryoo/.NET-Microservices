952c8cf9201ea8b2821e1ed2383736931d78bd63
Arthur Ryoo <jene0523@gmail.com>
2022-03-14 22:38:44 -0700

    Fix method name to remove

--- Play.Common/src/Play.Common/IRepository.cs
+++ Play.Common/src/Play.Common/IRepository.cs
@@ -8,7 +8,7 @@ namespace Play.Common
     public interface IRepository<T> where T : IEntity
     {
         Task CreateAsync(T entity);
-        Task DeleteAsync(Guid id);
+        Task RemoveAsync(Guid id);
         Task<IReadOnlyCollection<T>> GetAllAsync();
         Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter);
         Task<T> GetAsync(Guid id);
